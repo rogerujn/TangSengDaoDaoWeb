8a121573497e59ee5f0f50e8146446e9e8553774
rogerujn <48398137+rogerujn@users.noreply.github.com>
2024-07-15 13:22:14 +0800

    Update index.tsx

--- apps/web/src/index.tsx
+++ apps/web/src/index.tsx
@@ -8,18 +8,18 @@ import { LoginModule } from "@tsdaodao/login";
 import { DataSourceModule } from "@tsdaodao/datasource";
 import { ContactsModule } from "@tsdaodao/contacts";
 
-const apiURL = "https://api.botgate.cn/v1/";
+const apiURL = "http://www.rogerujn.top:8090/v1/";
 
 if ((window as any).__TAURI_IPC__) {
   // tauri环境
   console.log("tauri环境");
-  WKApp.apiClient.config.apiURL = "http://www.rogerujn.top:8090/v1/";
+  WKApp.apiClient.config.apiURL = apiURL;
 } else if ((window as any)?.__POWERED_ELECTRON__) {
   console.log("__POWERED_ELECTRON__环境");
-  WKApp.apiClient.config.apiURL = "http://www.rogerujn.top:8090/v1/";
+  WKApp.apiClient.config.apiURL = apiURL;
 } else {
   if (process.env.NODE_ENV === "development") {
-    WKApp.apiClient.config.apiURL = "http://www.rogerujn.top:8090/v1/";
+    WKApp.apiClient.config.apiURL = apiURL;
   } else {
     WKApp.apiClient.config.apiURL = "/api/v1/"; // 正式环境地址 (通用打包镜像，用此相对地址),打包出来的镜像可以通过API_URL环境变量来修改API地址
   }
